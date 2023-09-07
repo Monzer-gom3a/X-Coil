c73137e1abf2e014f2fa6859f7594e131b92c6a9
Monzer-gom3a <MonzerMohammed9955@gmail.com>
2023-09-07 21:00:25 +0300

    add Home_Page_View file

---
+++ lib/featrues/home/presentation/view/Home_Page_View.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home_Page_View extends StatelessWidget {
+  const Home_Page_View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
