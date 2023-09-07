2ee79d3661ce1a1ed33d0dc1afa2ea7b2214a3cb
Monzer-gom3a <MonzerMohammed9955@gmail.com>
2023-09-07 21:33:04 +0300

    add AppBar²

--- lib/featrues/home/presentation/view/Home_Page_View.dart
+++ lib/featrues/home/presentation/view/Home_Page_View.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home_Page_View extends StatelessWidget {
@@ -5,6 +6,95 @@ class Home_Page_View extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 226, 227, 230),
+          appBar: AppBar(
+            bottom: const TabBar(
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorColor: Color(0xE9D40B0B),
+              tabs: [
+                Tab(
+                  child: Text("محركات 1 فاز",
+                      style: TextStyle(
+                          //fontSize: 17,
+                          fontFamily: 'cairo',
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 2, 2, 2))),
+                ),
+                Tab(
+                  child: Text(
+                    "محركات 3 فازات",
+                    style: TextStyle(
+                        //fontSize: 17,
+                        fontFamily: 'cairo',
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 2, 2, 2)),
+                  ),
+                ),
+              ],
+            ),
+            title: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(233, 212, 11, 11),
+                        borderRadius: BorderRadius.circular(10)),
+                    //  width: double.infinity,
+                    child: Stack(
+                      children: [
+                        Row(
+                          children: [
+                            /* IconButton(
+                              tooltip: "القائمة الجانبية",
+                              onPressed: () {},
+                              icon: Image.asset(
+                                'assets/menu.png',
+                                color: const Color.fromARGB(255, 255, 255, 255),
+                                width: 23,
+                              ),
+                            ), */
+                            const Spacer(),
+                            IconButton(
+                              tooltip: "البحث",
+                              color: Colors.white,
+                              onPressed: () {},
+                              icon: const Row(
+                                children: [
+                                  // const Icon(Icons.search),
+                                  Text(
+                                    "ابحث في X-Coil ",
+                                    style: TextStyle(
+                                        //fontSize: 17,
+                                        fontFamily: 'cairo',
+                                        fontWeight: FontWeight.bold,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Spacer(),
+                            IconButton(
+                              tooltip: "مزامنة مع الانترنت",
+                              onPressed: () {},
+                              icon: const Icon(CupertinoIcons.cloud_download),
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                              iconSize: 23,
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            elevation: 0,
+            centerTitle: true,
+          ),
+        ));
   }
 }
